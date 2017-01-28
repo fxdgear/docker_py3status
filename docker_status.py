d38c0c4ae875a19996c9b36492afee7b19edc15c
Nick Lang <nick.lang@gmail.com>
2017-01-27 17:29:16 -0700

    Create docker_status.py

---
+++ docker_status.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+This module will produce a ratio of running to all docker containers
+"""
+import docker
+import subprocess
+import sys
+
+
+class Py3status:
+
+    template = '🐳 {version} Containers: {running}/{total} Images: {images} Swarm: {managers}/{nodes}'
+
+    def __init__(self):
+        self.client = docker.from_env()
+
+    def docker_status(self):
+        containers = self._get_container()
+        images = self._get_images()
+        swarm = self._get_swarm()
+        version = self._get_version()
+
+        full_text = self.template.format(
+                version=version,
+                running=containers["running"],
+                total=containers["total"],
+                images=images["images"],
+                managers=swarm["managers"],
+                nodes=swarm["nodes"]
+            )
+
+        return {
+            'full_text': full_text,
+            'cached_until': self.py3.time_in(1)
+        }
+
+    def _get_info(self):
+        return self.client.info()
+
+
+    def _get_container(self):
+        info = self._get_info()
+        total_containers = info['Containers']
+        running_containers = info['ContainersRunning']
+        return {"running": running_containers, "total": total_containers}
+
+
+    def _get_version(self):
+        info = self._get_info()
+        return info['ServerVersion']
+
+
+    def _get_images(self):
+        info = self._get_info()
+        return {"images": info["Images"]}
+
+
+    def _get_swarm(self):
+        info = self._get_info()
+        if not bool(info["Swarm"]['Cluster']['ID']):
+            return {}
+        return {"managers": info["Swarm"]["Managers"], "nodes": info["Swarm"]["Nodes"]}
+
